df3763074883a0fb35fe066305e4a1f0ea50f36d
Jasper Hall <jasperroony@icloud.com>
2025-09-11 14:57:06 -0400

    docs: answered Task B step 1; feat: Added additional test for basic-parser

--- tests/basic-parser.test.ts
+++ tests/basic-parser.test.ts
@@ -2,9 +2,10 @@ import { parseCSV } from "../src/basic-parser";
 import * as path from "path";
 
 const PEOPLE_CSV_PATH = path.join(__dirname, "../data/people.csv");
+const ROMAN_CSV_PATH = path.join(__dirname, "../data/roman.csv");
 
 test("parseCSV yields arrays", async () => {
-  const results = await parseCSV(PEOPLE_CSV_PATH)
+  const results = await parseCSV(PEOPLE_CSV_PATH);
   
   expect(results).toHaveLength(5);
   expect(results[0]).toEqual(["name", "age"]);
@@ -15,8 +16,17 @@ test("parseCSV yields arrays", async () => {
 });
 
 test("parseCSV yields only arrays", async () => {
-  const results = await parseCSV(PEOPLE_CSV_PATH)
+  const results = await parseCSV(PEOPLE_CSV_PATH);
+
   for(const row of results) {
     expect(Array.isArray(row)).toBe(true);
   }
+});
+
+test("parseCSV commas in quotations retained", async () => {
+  const results = await parseCSV(ROMAN_CSV_PATH);
+
+  expect(results).toHaveLength(2);
+  expect(results[0]).toEqual(['Caesar']);
+  expect(results[1]).toEqual(['Julius','veni, vidi, vici']);
 });
